376ab692506436acdf8b1ee9bd2cc6d471faaad1
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-05-28 09:37:49 -0400

    fix prototype reference

--- source/utilities/elements.js
+++ source/utilities/elements.js
@@ -1,5 +1,5 @@
 import { Component } from "../elements/component";
 
-const kebabType = (type) => type === Component ? 'component' : `${type.name.toLowerCase()}-${kebabType(type.prototype)}`;
+const kebabType = (type) => type === Component ? 'component' : `${type.name.toLowerCase()}-${kebabType(Object.getPrototypeOf(type))}`;
 
 export const define = (type) => customElements.define(kebabType(type), type);
