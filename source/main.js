539996ae304bec3de5bdbd8a845dcf5ba11e00cb
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-05-31 09:28:25 -0400

    update broken export

--- source/main.js
+++ source/main.js
@@ -1,6 +1,6 @@
 export * from './aliases/document.js';
 export * from './aliases/elements.js';
 export * from './aliases/events.js';
-export * from './elements/component.js';
+export * from './elements/quantum.js';
 export * from './utilities/elements.js';
 export * from './utilities/templates.js';
