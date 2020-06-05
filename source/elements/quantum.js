b5c92604c06433129be314573e5c07511feb9af2
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-06-05 09:48:07 -0400

    update event abstraction

--- source/elements/quantum.js
+++ source/elements/quantum.js
@@ -2,7 +2,6 @@ import { append, query } from '../aliases/element.js';
 import { keys } from '../aliases/object.js';
 import { defineAttributes } from '../utilities/attributes.js';
 import { iterate, map } from '../utilities/objects.js';
-import { dispatcher } from '../utilities/events.js';
 import { shadow } from '../utilities/elements.js';
 import { clone } from '../utilities/templates.js';
 
@@ -18,7 +17,7 @@ export class Quantum extends HTMLElement {
         const constructor = this.constructor;
         const elements = map(constructor.elements, selector => query(root, selector));
         this.#renderers = map(constructor.attributes, renderer => renderer(elements));
-        iterate(constructor.events, (event, delegate) => delegate(elements, dispatcher(this, event)));
+        iterate(constructor.events, (event, delegate) => delegate(this, elements, event));
 
         // const slots = root.querySelectorAll('slot');
         // for (const slot of slots) {
--- source/utilities/events.js
+++ source/utilities/events.js
@@ -1,3 +1,1 @@
 export const dispatch = (element, event, options) => element.dispatchEvent(new CustomEvent(event, { cancelable: true, ...options }));
-
-export const dispatcher = (element, event) => options => dispatch(element, event, options);
