709426ec511600806c54d6fb7fce888611edbcd0
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-06-05 09:57:34 -0400

    redo event changes

--- source/elements/quantum.js
+++ source/elements/quantum.js
@@ -2,6 +2,7 @@ import { append, query } from '../aliases/element.js';
 import { keys } from '../aliases/object.js';
 import { defineAttributes } from '../utilities/attributes.js';
 import { iterate, map } from '../utilities/objects.js';
+import { dispatcher } from '../utilities/events.js';
 import { shadow } from '../utilities/elements.js';
 import { clone } from '../utilities/templates.js';
 
@@ -17,7 +18,7 @@ export class Quantum extends HTMLElement {
         const constructor = this.constructor;
         const elements = map(constructor.elements, selector => query(root, selector));
         this.#renderers = map(constructor.attributes, renderer => renderer(elements));
-        iterate(constructor.events, (event, delegate) => delegate(this, elements, event));
+        iterate(constructor.events, (event, delegate) => delegate(this, elements, dispatcher(this, event)));
 
         // const slots = root.querySelectorAll('slot');
         // for (const slot of slots) {
--- source/utilities/events.js
+++ source/utilities/events.js
@@ -1,1 +1,3 @@
 export const dispatch = (element, event, options) => element.dispatchEvent(new CustomEvent(event, { cancelable: true, ...options }));
+
+export const dispatcher = (element, event) => options => dispatch(element, event, options);
