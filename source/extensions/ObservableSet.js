150dfbaf71cb95ce9bdc09c1dbf546eef59fa93b
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-06-08 10:49:44 -0400

    change observable set event

--- source/extensions/ObservableSet.js
+++ source/extensions/ObservableSet.js
@@ -1,24 +1,17 @@
 export class ObservableSet extends Set {
     constructor(iterable) {
         super(iterable);
-
-        this.onAdd = new Set();
-        this.onDelete = new Set();
     }
 
     add(value) {
         if (super.add(value)) {
-            for (const listener of this.onAdd) {
-                listener(value);
-            }
+            this?.onAdd(value);
         }
     }
 
     delete(value) {
         if (super.delete(value)) {
-            for (const listener of this.onDelete) {
-                listener(value);
-            }
+            this?.onDelete(value);
         }
     }
 }
