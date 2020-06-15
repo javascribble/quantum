eeb325abf784fd5b266113be35713aae356d50b5
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-06-15 09:07:00 -0400

    add event dataTransfer alias

--- source/aliases/object.js
+++ source/aliases/object.js
@@ -2,9 +2,6 @@ export const keys = Object.keys;
 export const values = Object.values;
 export const assign = Object.assign;
 export const entries = Object.entries;
+export const fromEntries = Object.fromEntries;
 export const defineProperty = Object.defineProperty;
 export const defineProperties = Object.defineProperties;
-
-export const fromEntries = Object.fromEntries;
-
-export const hasOwnProperty = (object, property) => object.hasOwnProperty(property);
