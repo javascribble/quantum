2e453b5a9b0a279c21168cef02536af91aacffb3
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-06-15 09:13:13 -0400

    remove that, too broad

--- source/aliases/event.js
+++ source/aliases/event.js
@@ -1,7 +1,3 @@
 export const preventDefault = (event) => event.preventDefault();
 
 export const stopPropagation = (event) => event.stopPropagation();
-
-export const getData = (event, key) => event.dataTransfer.getData(key);
-
-export const setData = (event, key, value) => event.dataTransfer.setData(key, value);
