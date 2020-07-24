472391e4160ed2e64ac40925955c3ca6009b6ade
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-07-24 10:29:48 -0400

    fix null reference

--- source/utilities/component.js
+++ source/utilities/component.js
@@ -2,11 +2,14 @@ import { getAttribute, setAttribute } from '../decorators/element.js';
 import { formatAttribute } from '../functions/format.js';
 
 export const define = (name, type) => {
-    for (const attribute of type?.observedAttributes) {
-        Object.defineProperty(type.prototype, formatAttribute(attribute), {
-            get() { return getAttribute(this, attribute); },
-            set(value) { setAttribute(this, attribute, value); }
-        });
+    const attributes = type.observedAttributes;
+    if (Array.isArray(attributes)) {
+        for (const attribute of attributes) {
+            Object.defineProperty(type.prototype, formatAttribute(attribute), {
+                get() { return getAttribute(this, attribute); },
+                set(value) { setAttribute(this, attribute, value); }
+            });
+        }
     }
 
     customElements.define(name, type);
