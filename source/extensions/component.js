fe4193d5529fbd506d0a57169ec865797e77d11c
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-07-27 07:54:38 -0400

    add slot listener to base

--- source/extensions/component.js
+++ source/extensions/component.js
@@ -13,7 +13,17 @@ export class Component extends HTMLElement {
 
         const root = shadow ? this.attachShadow({ mode }) : this;
         if (template) {
-            root.appendChild((template.content || template).cloneNode(true));
+            root.appendChild(template.content.cloneNode(true));
+            for (const slot of root.querySelectorAll('slot')) {
+                let elements = []; // This could be public
+                slot.addEventListener('slotchange', event => {
+                    const assignedElements = slot.assignedElements();
+                    const addedElements = assignedElements.filter(element => !elements.includes(element));
+                    const deletedElements = elements.filter(element => !assignedElements.includes(element));
+                    this?.slotChangedCallback(slot, addedElements, deletedElements, assignedElements);
+                    elements = assignedElements;
+                });
+            }
         }
     }
 }
--- source/utilities/template.js
+++ source/utilities/template.js
@@ -3,13 +3,3 @@ export const template = html => {
     element.innerHTML = html;
     return element;
 };
-
-export const slotListener = (slot, onAdd, onDelete) => {
-    let elements = [];
-    return event => {
-        const assignedElements = slot.assignedElements();
-        assignedElements.filter(element => !elements.includes(element)).forEach(onAdd);
-        elements.filter(element => !assignedElements.includes(element)).forEach(onDelete);
-        elements = assignedElements;
-    };
-};
