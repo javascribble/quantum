a3f906ffe0992c2759163a2107c12068b53eb57b
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-10-15 09:34:48 -0400

    add global assignment

--- source/main.js
+++ source/main.js
@@ -1,25 +1,3 @@
-import * as eventCollections from './collections/event.js';
-import * as optionsConstants from './constants/options.js';
-import * as elementDecorators from './decorators/element.js';
-import * as componentElements from './elements/component.js';
-import * as numberFunctions from './functions/number.js';
-import * as stringFunctions from './functions/string.js';
-import * as animationUtilities from './utilities/animation.js';
-import * as componentUtilities from './utilities/component.js';
-import * as templateUtilities from './utilities/template.js';
-
-window.quantum = Object.assign(window.quantum || {}, {
-    ...eventCollections,
-    ...optionsConstants,
-    ...elementDecorators,
-    ...componentElements,
-    ...numberFunctions,
-    ...stringFunctions,
-    ...animationUtilities,
-    ...componentUtilities,
-    ...templateUtilities
-});
-
 export * from './collections/event.js';
 export * from './constants/options.js';
 export * from './decorators/element.js';
