84cbb57dc76964ec473cdf25a9cc2e6e6257b0c5
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-10-16 13:17:42 -0400

    improve readability

--- source/utilities/visibility.js
+++ source/utilities/visibility.js
@@ -1,9 +1,9 @@
 const visible = 'visible', hidden = 'hidden';
 
-export const show = style => (style || style.style).visibility = visible;
+export const show = style => style.visibility = visible;
 
-export const hide = style => (style || style.style).visibility = hidden;
+export const hide = style => style.visibility = hidden;
 
-export const shown = style => (style || style.style).visibility == visible;
+export const shown = style => style.visibility == visible;
 
 export const toggle = style => shown(style) ? hide(style) : show(style);
