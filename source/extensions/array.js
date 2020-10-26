b136012d42494300ab4eacb4202ff0e04c4a0494
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-10-26 09:18:01 -0400

    add remove function

--- source/extensions/array.js
+++ source/extensions/array.js
@@ -1,3 +1,13 @@
 Array.prototype.last = function () {
     return this.length > 0 ? this[this.length - 1] : null;
 };
+
+Array.prototype.remove = function (element) {
+    const index = this.indexOf(element);
+    if (index) {
+        this.splice(index, 1);
+        return true;
+    }
+
+    return false;
+};
