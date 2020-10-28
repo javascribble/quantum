ddcdf9bb48686fbac91b50ecf06c5e5f633c4a3e
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-10-28 07:35:54 -0400

    syntax error

--- source/primitives/string.js
+++ source/primitives/string.js
@@ -1,3 +1,3 @@
-export const generateId = (length = 32, radix = 16) => length ? (Math.random() * radix | 0).toString(radix) + randomString(length - 1) : '';
+export const generateId = (length = 32, radix = 16) => length ? (Math.random() * radix | 0).toString(radix) + generateId(length - 1) : '';
 
 export const formatAttribute = attribute => attribute.replace(/[^a-zA-Z0-9]+(.)/g, (match, character) => character.toUpperCase());
