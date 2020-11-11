faca57833dcfa9d0012ef554b105b8e7f329f740
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-11-11 08:11:39 -0500

    add array extensions

--- source/extensions/array.js
+++ source/extensions/array.js
@@ -1,8 +1,12 @@
-Array.prototype.last = function () {
-    return this.length > 0 ? this[this.length - 1] : null;
-};
-
 Array.prototype.remove = function (element) {
     const index = this.indexOf(element);
     return index > -1 ? this.splice(index, 1)[0] : null;
+};
+
+Array.prototype.difference = function (domain) {
+    return this.filter(element => !domain.includes(element));
+};
+
+Array.prototype.intersection = function (domain) {
+    return this.filter(element => domain.includes(element));
 };
