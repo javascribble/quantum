e3428a9a59388941d41e974dcc02d846085d7ab1
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-01-27 11:24:19 -0500

    remove extra loaders

--- source/utilities/resources.js
+++ source/utilities/resources.js
@@ -2,24 +2,17 @@ export const loadJson = (url, options) => fetch(url, options).then(response => r
 
 export const loadText = (url, options) => fetch(url, options).then(response => response.text());
 
-export const loadBlob = (url, options) => fetch(url, options).then(response => response.blob());
-
-export const loadFormData = (url, options) => fetch(url, options).then(response => response.formData());
-
-export const loadArrayBuffer = (url, options) => fetch(url, options).then(response => response.arrayBuffer());
-
 export const loadImage = (url, options) => new Promise((resolve, reject) => {
     const image = new Image();
     image.onload = () => resolve(image);
+    image.onerror = () => reject(image);
     image.src = url;
 });
 
 export const loaders = {
     json: loadJson,
     txt: loadText,
-    bin: loadBlob,
-    png: loadImage,
-    bin: loadArrayBuffer
+    png: loadImage
 };
 
 export const load = (url, options) => loaders[url.substring(url.lastIndexOf('.') + 1)](url, options);
