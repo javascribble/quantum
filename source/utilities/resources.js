669833fdb59a8ad74f07427009f55afde0dc5ffe
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-01-28 11:14:44 -0500

    simplify loader

--- source/utilities/resources.js
+++ source/utilities/resources.js
@@ -2,19 +2,9 @@ export const loadJson = (url, options) => fetch(url, options).then(response => r
 
 export const loadText = (url, options) => fetch(url, options).then(response => response.text());
 
-export const loadImage = (url, options) => new Promise((resolve, reject) => {
-    const image = new Image();
-    image.onload = () => resolve(image);
-    image.onerror = () => reject(image);
-    image.src = url;
-});
-
 export const loaders = {
     json: loadJson,
-    txt: loadText,
-    png: loadImage
+    txt: loadText
 };
 
 export const load = (url, options) => loaders[url.substring(url.lastIndexOf('.') + 1)](url, options);
-
-export const loadMany = (urls, options) => Promise.all(urls.map(url => load(url, options)));
