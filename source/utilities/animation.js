975dd32d827ba876991371a9217d48fb668d42d7
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-02-22 08:49:02 -0500

    clean up animation method

--- source/utilities/animation.js
+++ source/utilities/animation.js
@@ -1,12 +1,11 @@
 export const animate = animation => {
-    const initial = performance.now();
-    const time = { delta: 0, elapsed: 0 };
-
     let frame = 0;
-    let previous = initial;
+    let previous = performance.now();
+    const time = { delta: 0, elapsed: 0 };
     const iterate = current => {
-        time.delta = current - previous;
-        time.elapsed = current - initial;
+        const delta = current - previous;
+        time.delta = delta;
+        time.elapsed += delta;
         if (animation(time)) {
             previous = current;
             frame = requestAnimationFrame(iterate);
