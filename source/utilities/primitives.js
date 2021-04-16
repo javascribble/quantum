59ee4a281406a92b311780421f684dfd601c036d
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-04-16 16:33:08 -0400

    move math utility methods

--- source/utilities/primitives.js
+++ source/utilities/primitives.js
@@ -1,7 +1,3 @@
 export const randomString = (length = 32, radix = 16) => length ? (Math.random() * radix | 0).toString(radix) + randomString(length - 1) : '';
 
 export const randomNumber = (max = 1, min = 0) => Math.random() * (min - max) + min;
-
-export const lerp = (number, prime, ratio) => number + (number - prime) * ratio;
-
-export const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
