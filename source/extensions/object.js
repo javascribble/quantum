957c5df2b7f5adf246ff7a69c82ddf67686d35a4
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-04-23 13:45:12 -0400

    rename arg

--- source/extensions/object.js
+++ source/extensions/object.js
@@ -1,3 +1,3 @@
-Object.prototype.mapEntries = function (delegate) {
-    return Object.fromEntries(Object.entries(this).map(delegate));
+Object.prototype.mapEntries = function (predicate) {
+    return Object.fromEntries(Object.entries(this).map(predicate));
 };
