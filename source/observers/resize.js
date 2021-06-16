ba53e9d3bbb62c8d557ec61a797de27ce229f405
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-06-16 08:11:42 -0400

    add entry to resize event

--- source/observers/resize.js
+++ source/observers/resize.js
@@ -3,19 +3,18 @@ import { eventOptions } from '../constants/options.js';
 const observed = new Set();
 
 const dispatch = () => {
-    const event = new CustomEvent('resize');
-    for (const element of observed) {
-        element.dispatchEvent(event);
+    for (const entry of observed) {
+        entry.target.dispatchEvent(new CustomEvent('resize', { detail: entry }));
     }
 
     observed.clear();
 };
 
 let timeout = 0;
-const observe = (entries, observer) => {
-    for (const { target } of entries) {
-        if (!observed.has(target)) {
-            observed.add(target);
+const observe = entries => {
+    for (const entry of entries) {
+        if (!observed.has(entry)) {
+            observed.add(entry);
         }
     }
 
