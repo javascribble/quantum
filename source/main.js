d4fb7250985a31e44c4d366c362bad9a54899a84
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 10:54:40 -0400

    update exports

--- source/main.js
+++ source/main.js
@@ -1,17 +1,16 @@
-import { Quantum } from './elements/quantum.js';
-
 import * as maps from './collections/maps.js';
 import * as sets from './collections/sets.js';
 import * as options from './constants/options.js';
 import * as element from './decorators/element.js';
 import * as templates from './document/templates.js';
+import * as quantum from './elements/quantum.js';
 import * as resize from './observers/resize.js';
 import * as numbers from './utilities/numbers.js';
 import * as strings from './utilities/strings.js';
 import * as animation from './window/animation.js';
 import * as resources from './window/resources.js';
 
-window.Quantum = Quantum;
+window.Quantum = quantum.Quantum;
 window.quantum = {
     ...maps,
     ...sets,
@@ -25,15 +24,14 @@ window.quantum = {
     ...resources
 };
 
-export default Quantum;
-
-export * from maps;
-export * from sets;
-export * from options;
-export * from element;
-export * from templates;
-export * from resize;
-export * from numbers;
-export * from strings;
-export * from animation;
-export * from resources;
+export * from './collections/maps.js';
+export * from './collections/sets.js';
+export * from './constants/options.js';
+export * from './decorators/element.js';
+export * from './document/templates.js';
+export * from './elements/quantum.js';
+export * from './observers/resize.js';
+export * from './utilities/numbers.js';
+export * from './utilities/strings.js';
+export * from './window/animation.js';
+export * from './window/resources.js';
