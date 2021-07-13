d368528838fd7882727fc75a71a71cbc58855828
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-13 08:29:00 -0400

    update export

--- bundles/main-window.js
+++ bundles/main-window.js
@@ -1,26 +1,4 @@
-import { Quantum } from '../source/elements/quantum.js';
+import * as main from './main.js';
 
-import * as maps from '../source/collections/maps.js';
-import * as sets from '../source/collections/sets.js';
-import * as options from '../source/constants/options.js';
-import * as element from '../source/decorators/element.js';
-import * as templates from '../source/document/templates.js';
-import * as resize from '../source/observers/resize.js';
-import * as numbers from '../source/utilities/numbers.js';
-import * as strings from '../source/utilities/strings.js';
-import * as animation from '../source/window/animation.js';
-import * as resources from '../source/window/resources.js';
-
-window.Quantum = Quantum;
-window.quantum = {
-    ...maps,
-    ...sets,
-    ...options,
-    ...element,
-    ...templates,
-    ...resize,
-    ...numbers,
-    ...strings,
-    ...animation,
-    ...resources
-};
+window.Quantum = main.Quantum;
+window.quantum = { ...main };
