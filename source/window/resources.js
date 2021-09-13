1115cfabc9de50466b1ba8ca76bbd2377cd351ac
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-09-13 15:16:56 -0400

    add load image

--- source/window/resources.js
+++ source/window/resources.js
@@ -2,9 +2,17 @@ export const loadJson = (url, options) => fetch(url, options).then(response => r
 
 export const loadText = (url, options) => fetch(url, options).then(response => response.text());
 
+export const loadImage = async (url, options) => {
+    const image = new Image();
+    image.src = url;
+    await image.decode();
+    return Promise.resolve(image);
+};
+
 export const loaders = {
     json: loadJson,
-    txt: loadText
+    txt: loadText,
+    png: loadImage
 };
 
 export const load = (url, options) => loaders[url.substring(url.lastIndexOf('.') + 1)](url, options);
