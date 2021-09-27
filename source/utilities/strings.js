e094451c01fb1db96016c11892a3adfd75f04a4a
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-09-27 08:18:16 -0400

    add string check method

--- source/utilities/strings.js
+++ source/utilities/strings.js
@@ -1,1 +1,3 @@
 export const randomString = (length = 32, radix = 16) => length ? (Math.random() * radix | 0).toString(radix) + randomString(length - 1) : '';
+
+export const isString = reference => typeof reference === 'string' || reference instanceof String;
